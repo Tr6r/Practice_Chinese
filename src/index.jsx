3cd603116e560e6f3366877b774ace70c9bf46ca
Đinh Quốc Cường <tr6r20@gmail.com>
2024-06-08 18:20:37 +0700

    test helmet

--- src/index.jsx
+++ src/index.jsx
@@ -3,6 +3,8 @@ import ReactDOM from 'react-dom/client';
 import { BrowserRouter as Router, Route, Switch, Redirect, Navigate, Routes } from 'react-router-dom';
 import './index.scss';
 import App from './App';
+import { Helmet } from 'react-helmet';
+
 import { ToastContainer, toast } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
 
@@ -11,6 +13,9 @@ root.render(
   <Router>
 
   <React.StrictMode>
+  <Helmet>
+          <meta name="viewport" content="width=device-width, initial-scale=1.0, viewport-fit=cover" />
+        </Helmet> 
     <App />
     <ToastContainer />
   </React.StrictMode>
